4d388eadbf7c31f9a3216de3b7fb87a038a1f483
Cavallaro Catello Stefano <stefano@stepo.biz>
2022-12-21 12:24:59 +0100

    versions update

---
+++ src/lib/stal-autocomplete.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'stal-autocomplete',
+  template: `
+    <div class="stal-autocomplete wrapper" *ngIf="show">
+      <div *ngFor="let element of filteredElements | async" (click)="select(element)">{{ displayFn(element) }}</div>
+    </div>
+  `,
+  styles: [`
+    .stal-autocomplete, .wrapper {
+      position: fixed;
+      background-color: #aaa;
+    }
+    .stal-autocomplete {
+      z-index: 999999;
+    }
+  `]
+})
+export class StalAutocompleteComponent {
+  @Input() filteredElements: Observable<any[]> | undefined;
+  @Input() input: HTMLInputElement | undefined;
+
+  @Input() displayFn: Function = (selectedElement: any) => {
+    return selectedElement.description;
+  };
+
+  selectedElementObservable: Observable<any>;
+  private selectedElement$ = new Subject<any>();
+  protected show: boolean = false;
+
+  constructor() {
+    this.selectedElementObservable = this.selectedElement$.asObservable();
+  }
+
+  select(e: any) {
+    this.selectedElement$.next(e);
+  }
+
+  showOptions(show: boolean) {
+    this.show = show;
+  }
+}
---
+++ src/lib/stal-autocomplete.directive.ts
@@ -0,0 +1,72 @@
+import { Directive, ElementRef, HostListener, Input, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup, NgControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { StalAutocompleteComponent } from './stal-autocomplete.component';
+
+@Directive({
+	selector: '[stalAutocomplete]'
+})
+export class StalAutocompleteDirective implements OnInit, OnDestroy {
+	@Input() stalAutocomplete: StalAutocompleteComponent | undefined;
+
+	@Input() displayFn: Function = (selectedElement: any) => {
+		return selectedElement.description;
+	};
+
+	constructor(
+		private el: ElementRef,
+		private ngControl: NgControl,
+	) { }
+
+	destroyer: Subscription[] = [];
+
+	ngOnInit() {
+        if(this.stalAutocomplete === undefined) throw new Error('stalAutocomplete param is required');
+
+		// Prepare variables
+		let inputElement: HTMLInputElement = this.el.nativeElement;
+
+		let form: FormGroup;
+		let controlName: string;
+		{
+			let formControlName: any = this.ngControl;
+			form = formControlName._parent.form;
+			controlName = formControlName.name;
+		}
+
+		// On start
+		if (form.value[controlName] !== undefined) {
+			if (form.value[controlName] != null) {
+				inputElement.value = this.displayFn(form.value[controlName]);
+			}
+		}
+
+		// On Edit event
+		this.destroyer.push(this.stalAutocomplete.selectedElementObservable.subscribe(
+			(selectedElement) => {
+				{
+					let patch: any = {};
+					patch[controlName] = selectedElement;
+					form.patchValue(patch);
+				}
+				inputElement.value = this.displayFn(selectedElement);
+			}
+		));
+	}
+
+	@HostListener('focus', ['$event']) onFocus(e: any) {
+        if(this.stalAutocomplete === undefined) throw new Error('stalAutocomplete param is required');
+		this.stalAutocomplete.showOptions(true);
+	}
+
+	@HostListener('focusout', ['$event']) onFocusOut(e: any) {
+        setTimeout(() => {
+            if(this.stalAutocomplete === undefined) throw new Error('stalAutocomplete param is required');
+			this.stalAutocomplete.showOptions(false);
+		}, 200);
+	}
+
+	ngOnDestroy() {
+		this.destroyer.forEach(s => s.unsubscribe());
+	}
+}
