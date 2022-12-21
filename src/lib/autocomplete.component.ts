275607a7955e1e1368688e78883ffa3cfb8307a6
Cavallaro Catello Stefano <stefano@stepo.biz>
2022-12-21 11:05:29 +0100

    init

---
+++ src/lib/autocomplete.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-autocomplete',
+  template: `
+    <p>
+      autocomplete works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class AutocompleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/lib/autocomplete.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { AutocompleteComponent } from './autocomplete.component';
+
+
+
+@NgModule({
+  declarations: [
+    AutocompleteComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    AutocompleteComponent
+  ]
+})
+export class AutocompleteModule { }
